473ac717fc62030efe831840cd692c7d1e1901a4
Ted S <tstaros23@gmail.com>
2021-07-13 08:23:50 -0600

    Finishing up iteration 1

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,14 +1,40 @@
 class Cell
-  attr_reader :coordinate, :ship
+  attr_reader :coordinate, :ship, :been_fired_upon
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @been_fired_upon = false
   end
 
   def empty?
     @ship == nil
   end
 
-  def place_ship
+  def place_ship(ship)
+    @ship = ship
+  end
+
+  def fired_upon?
+    @been_fired_upon
+  end
+
+  def fire_upon
+    @been_fired_upon = true
+    if @ship != nil
+      @ship.hit
+    end
+  end
+
+  def render(show_ship=false)
+    if @been_fired_upon == false
+      "."
+    elsif @been_fired_upon == false && show_ship == true && @ship != nil
+      "S"
+    elsif @ship == nil && @been_fired_upon == true
+      "M"
+    
+
+
+
   end
 end
