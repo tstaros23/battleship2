ae6b27d226c4b2570e51ebea17d5a0d5ec076f44
Jamie Pace <jamiejpace@gmail.com>
2021-07-15 12:15:55 -0600

    add valid placement method

--- lib/board.rb
+++ lib/board.rb
@@ -11,4 +11,30 @@ class Board
   def valid_coordinate?(cell)
     @cells.include?(cell)
   end
+
+  def valid_placement?(ship, desired_coordinates)
+    split_coordinates = desired_coordinates.map do |coordinate|
+      coordinate.split("")
+    end
+    coordinate_letters = split_coordinates.map do |coordinate|
+      coordinate[0].ord.to_i
+    end
+    coordinate_numbers = split_coordinates.map do |coordinate|
+      coordinate[1].to_i
+    end
+    consecutive_letters = coordinate_letters.each_cons(2).all? { |x, y| y == x + 1 }
+    consecutive_numbers = coordinate_numbers.each_cons(2).all? { |x, y| y == x + 1 }
+
+    valid_length = ship.length == desired_coordinates.length
+
+    if valid_length != true
+      false
+    elsif consecutive_letters && coordinate_numbers.uniq.length == 1
+      true
+    elsif coordinate_letters.uniq.length == 1 && consecutive_numbers
+      true
+    else
+      false
+    end
+  end
 end
