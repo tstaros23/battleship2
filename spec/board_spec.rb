5a94b9a801ef1e6d3275c07e2450c47459aca9b6
Jamie Pace <jamiejpace@gmail.com>
2021-07-16 12:42:44 -0600

    Change cells variable and update valid placement and place ship

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,5 @@
 require './lib/board'
 require './lib/ship'
-require './lib/cell'
 
 RSpec.describe Board do
   it 'exists' do
@@ -20,8 +19,6 @@ RSpec.describe Board do
   # end
   it 'validates coordinates' do
     board = Board.new
-    cell_1 = Cell.new("B4")
-    board.add_cell("B4")
 
     expect(board.valid_coordinate?("B4")).to eq(true)
     expect(board.valid_coordinate?("A23")).to eq(false)
@@ -33,54 +30,61 @@ RSpec.describe Board do
       submarine = Ship.new("Submarine", 2)
 
       expect(board.split_coordinates(["A2", "A3", "A4"])).to eq([["A", "2"], ["A", "3"], ["A", "4"]])
-    end
+  end
 
-    it 'organizes by index into seperate arrays' do
-      board = Board.new
-      cruiser = Ship.new("Cruiser", 3)
-      submarine = Ship.new("Submarine", 2)
-      array_of_coordinates = board.split_coordinates(["A2", "A3", "A4"])
+  it 'organizes by index into seperate arrays' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    array_of_coordinates = board.split_coordinates(["A2", "A3", "A4"])
 
-      expect(board.organize_letters_by_index(array_of_coordinates, 0)).to eq([65, 65, 65])
-    end
+    expect(board.organize_letters_by_index(array_of_coordinates, 0)).to eq([65, 65, 65])
+  end
+
+  it 'can tell if a cell is open' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser,["A1", "A2", "A3"])
+
+    expect(board.cells_open?(["A1", "A2"])).to be false
+    expect(board.cells_open?(["B1", "B2"])).to be true
+  end
 
 
-    it 'validates placement' do
-      board = Board.new
-      cruiser = Ship.new("Cruiser", 3)
-      submarine = Ship.new("Submarine", 2)
+  it 'validates placement' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
 
 
-      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
-      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
-      expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
-      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
-      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
-      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
-      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
-      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
-      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
-      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
-    end
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be false
+    expect(board.valid_placement?(submarine, ["A2", "A3"])).to be true
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+    expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+    expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+  end
 
-    it "places a ship" do
-      board = Board.new
-      cruiser = Ship.new("Cruiser", 3)
-      board.add_cell("A1")
-      board.add_cell("A2")
-      board.add_cell("A3")
+  it "places a ship" do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
 
-      board.place(cruiser,["A1", "A2", "A3"])
 
-      cell_1 = board.cells["A1"]
-      cell_2 = board.cells["A2"]
-      cell_3 = board.cells["A3"]
+    board.place(cruiser,["A1", "A2", "A3"])
 
-      # require "pry"; binding.pry
-      expect(cell_1.ship).to eq(cruiser)
-      expect(cell_2.ship).to eq(cruiser)
-      expect(cell_3.ship).to eq(cruiser)
-      expect(cell_3.ship == cell_2.ship).to eq(true)
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
 
-    end
+
+    expect(cell_1.ship).to eq(cruiser)
+    expect(cell_2.ship).to eq(cruiser)
+    expect(cell_3.ship).to eq(cruiser)
+    expect(cell_3.ship == cell_2.ship).to eq(true)
+  end
 end
